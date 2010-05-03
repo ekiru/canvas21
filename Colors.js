1c33affb6a00b85c0977e885b7d17adc1c894e3d
Tyler Leslie Curtis <ekiru.0@gmail.com>
2010-05-03 15:45:30 -0500

    Converted it to a working, if not very pretty canvas game.

---
+++ Colors.js
@@ -0,0 +1,6 @@
+var Colors = {
+	CardBorder: '#000000',
+	CardInterior: '#EEEEEE',
+	RedColor: '#FF0000',
+	BlackColor: '#000000'
+};
--- canvas21.js
+++ canvas21.js
@@ -11,6 +11,61 @@ var Canvas21 = {};
 	Card.Ranks = new Enum(11, 'Jack', 'Queen', 'King', 'Ace');
 	Card.Suits = new Enum('Clubs', 'Diamonds', 'Hearts', 'Spades');
 
+	Card.Width = 30;
+	Card.Height = 20;
+
+	Card.prototype.isRed = function () {
+		return this.suit === Card.Suits.Diamonds 
+			|| this.suit == Card.Suits.Hearts
+	};
+
+	Card.prototype.render = function (ctx, x, y) {
+		var oldFill = ctx.fillStyle;
+		var oldStroke = ctx.strokeStyle;
+		ctx.fillStyle = Colors.CardInterior;
+		ctx.fillRect(x, y, Card.Width, Card.Height);
+		ctx.strokeStyle = Colors.CardBorder;
+		ctx.strokeRect(x, y, Card.Width, Card.Height);
+
+		if (this.isRed()) {
+			ctx.fillStyle = Colors.RedColor;
+		} else {
+			ctx.fillStyle = Colors.BlackColor;
+		}
+		ctx.fillText(this.rankString() + this.suitString(), x + 5, y + 10);
+
+		ctx.fillStyle = oldFill;
+		ctx.strokeStyle = oldStroke;
+	};
+
+	Card.prototype.rankString = function () {
+		switch (this.rank) {
+		case Card.Ranks.Ace:
+			return 'A';
+		case Card.Ranks.King:
+			return 'K';
+		case Card.Ranks.Queen:
+			return 'Q';
+		case Card.Ranks.Jack:
+			return 'J';
+		default:
+			return this.rank.toString();
+		}
+	};
+
+	Card.prototype.suitString = function () {
+		switch (this.suit) {
+		case Card.Suits.Clubs:
+			return '\u2663';
+		case Card.Suits.Diamonds:
+			return '\u2666';
+		case Card.Suits.Hearts:
+			return '\u2665';
+		case Card.Suits.Spades:
+			return '\u2660';
+		}
+	}
+
 	Card.prototype.toString = function () {
 		var rank;
 		var suit;
@@ -64,7 +119,7 @@ var Canvas21 = {};
 	Module.export(Canvas21, "Deck", Deck);
 
 	Deck.prototype.shuffle = function () {
-		for (var i = this.cards.length; i > 1; i--) {
+		for (var i = this.cards.length - 1; i > 1; i--) {
 			var j = Math.round(Math.random() * i);
 			var tmp = this.cards[i];
 			this.cards[i] = this.cards[j];
@@ -73,10 +128,18 @@ var Canvas21 = {};
 	};
 
 	Deck.prototype.draw = function () {
-		return this.cards.pop();
+		var result = this.cards.pop();
+		if (result === undefined) {
+			window.deck = this;
+			throw new Error(["Undefined draw...", this]);
+		}
+		return result;
 	};
 
 	function Game () {
+		this.canvas = document.getElementById('gameCanvas');
+		this.ctx = this.canvas.getContext('2d');
+
 		this.deck = new Deck();
 		this.deck.shuffle();
 
@@ -89,41 +152,48 @@ var Canvas21 = {};
 		this.hit(this.dealer);
 
 		this.render();
+		console.log('Game initialized.');
 	}
 	Module.export(Canvas21, 'Game', Game);
+
+	Game.ResultX = 10;
+	Game.ResultY = 10;
+
+	Game.PlayerX = 60;
+	Game.PlayerY = 30;
+
+	Game.DealerX = 60;
+	Game.DealerY = 60;
 
 	Game.prototype.dealerCanPlay = function () {
 		return this.score(this.dealer) <= 17;
 	};
 
 	Game.prototype.render = function () {
-		var playerRow = document.getElementById('player');
-		var playerScore = document.getElementById('playerScore');
-		var dealerRow = document.getElementById('dealer');
-		var dealerScore = document.getElementById('dealerScore');
+		this.ctx.fillText("Player", 0, Game.PlayerY);
+		this.ctx.fillText("Dealer", 0, Game.DealerY);
 
+		var ctx = this.ctx;
+		var x = Game.PlayerX;
+		var y = Game.PlayerY;
 		Iter.map(this.player, function (item) {
-				if (!item.shown) {
-					var cardElement = document.createElement('td');
-					cardElement.innerHTML = item.card.toString();
-					playerRow.appendChild(cardElement);
-					item.shown = true;
-				}
+				if (!item.card) console.log(item);
+				item.card.render(ctx, x, y);
+				x += Card.Width + 10;
 			});
 
+		var x = Game.DealerX;
+		var y = Game.DealerY;
 		Iter.map(this.dealer, function (item) {
-				if (!item.shown) {
-					var cardElement = document.createElement('td');
-					cardElement.innerHTML = item.card.toString();
-					dealerRow.appendChild(cardElement);
-					item.shown = true;
-				}
+				if (!item.card) console.log(item);
+				item.card.render(ctx, x, y);
+				x += Card.Width + 10;
 			});
 
 		var winner;
 		if (winner = this.whoWon()) {
-			var resultElement = document.getElementById('result');
-			resultElement.innerHTML = 'The ' + winner + ' won.';
+			this.ctx.fillText('The ' + winner + ' won.', 
+				Game.ResultX, Game.ResultY);
 		}
 	}
 
@@ -136,7 +206,7 @@ var Canvas21 = {};
 			var card = this.deck.draw();
 			hand.push({
 				card: card,
-				hidden: (options && options.hidden) ? true : false
+				hidden: ((options && options.hidden) ? true : false)
 				});
 		} else {
 			throw new Error("Can't hit when the deck is empty.");
@@ -185,11 +255,11 @@ var Canvas21 = {};
 		if (dealerScore === 21) {
 			return 'dealer';
 		} else if (playerScore === 21) {
-			return 'player';		
-		} else if (this.hasBusted(this.dealer)) {
 			return 'player';
 		} else if (this.hasBusted(this.player)) {
-			return 'dealer';
+			return 'dealer';		
+		} else if (this.hasBusted(this.dealer)) {
+			return 'player';
 		} else if (!this.dealerCanPlay() && playerScore > dealerScore) {
 			return 'player';
 		} else {
