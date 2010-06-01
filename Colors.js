2f3ddcd7e855236fa328f0fdc351852a736ce94d
Tyler Leslie Curtis <ekiru.0@gmail.com>
2010-06-01 01:08:56 -0500

    Create branch.

--- Colors.js
+++
@@ -1,7 +0,0 @@
-var Colors = {
-	CardBack: '#330066',
-	CardBorder: '#000000',
-	CardInterior: '#EEEEEE',
-	RedColor: '#FF0000',
-	BlackColor: '#000000'
-};
--- Module.js
+++
@@ -1,9 +0,0 @@
-var Module = {};
-
-(function () {
-	// Helper for exporting.
-	function _export (module, name, val) {
-		module[name] = val;
-	}
-	_export(Module, '_export', _export);
-})();
